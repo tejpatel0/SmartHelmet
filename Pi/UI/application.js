1d5980f42ee798221936a246a5abf0765b5420b9
Tej Patel <tejpatel98@gmail.com>
2021-02-12 02:47:15 -0500

    Bug fixes and optimizations.

--- Pi/UI/application.js
+++ Pi/UI/application.js
@@ -6,14 +6,24 @@ var port = 8000;
 
 
 server.listen(port, () => console.log(`API server running on  ${port}!`))
+app.options('*', cors())
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/assets/jquery.js', (req, res) => {
+    res.sendFile(__dirname + '/assets/jquery.js')
+})
+
 app.get('/api/display/:status', (req, res) => {
-    var status = req.params('status')
+    var status = req.param('status')
     if(status == 'on'){
         shell.exec('sudo systemctl start sensing.service')
         res.json({success:"display turned on for detection"})
