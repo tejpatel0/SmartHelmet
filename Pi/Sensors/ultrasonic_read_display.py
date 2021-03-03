05cd044c139cfb10339c0a12c1b9711dc601612f
Tej Patel <tejpatel98@gmail.com>
2021-03-02 22:05:38 -0500

    Add fill to 3 ft alert and add 5th sensor

--- Pi/Sensors/ultrasonic_read_display.py
+++ Pi/Sensors/ultrasonic_read_display.py
@@ -8,17 +8,19 @@ import time
 serial = i2c(port=1, address=0x3C)                                      # Set I2C interface
 device = ssd1309(serial)                                                # load the display reference
 GPIO.setmode (GPIO.BCM)                                                 # Set the GPIO mode to BCM numbering
-output_ports = [23,8,5,13]                                                      # Define the GPIO output port numbers
-input_ports = [24,7,6,19]                                                      # Define the GPIO input port numbers
+output_ports = [23,8,5,13,21]                                           # Define the GPIO output port numbers
+input_ports = [24,7,6,19,20]                                            # Define the GPIO input port numbers
 
-trig1=23                                                                  # set trigger port 1
-echo1=24                                                                 # set echo port 1
-trig2=13                                                                 # set trig port 2
-echo2=19                                                                 # set echo port 2
-trig3=5                                                                 # set trig port 3
-echo3=6                                                                 # set echo port 3
-trig4=8                                                                 # set trig port 4
-echo4=7                                                                 # set trig port 2                                                            # set echo port
+trig2=23                 # mid left                                       
+echo2=24                                                                 
+trig1=13                 # far left                                       
+echo1=19                                                                 
+trig3=5                                                                 
+echo3=6                                                                 
+trig4=8                                                                 
+echo4=7                                                                
+trig5=21                 # far right                                    
+echo5=20                                                                
 
 # HC-SR04 ULTRASONIC DISTANCE SENSOR                                *
 # *******************************************************************
@@ -83,7 +85,7 @@ def get_distance(echo, trig):
     distance = (time2 - time1) / 0.00000295 / 2 / 10                    # Convert the timer values into centimetres
     return (distance)                                                   # Exit with the distance in centimetres
 
-def draw_display(dist1, dist2, dist3, dist4):
+def draw_display(dist1, dist2, dist3, dist4, dist5):
     try:
         with canvas(device) as draw:
             # First we check for 9-12 ft range, then 6-9, then 0-6.
@@ -91,35 +93,42 @@ def draw_display(dist1, dist2, dist3, dist4):
             #print("sensor 2 is:", dist2)
             #print("sensor 3 is:", dist3)
             #print("sensor 4 is:", dist4)
-            if(dist1<350 and dist1>275):
+            if(dist1<350 and dist1>275 and dist1>0):
                 draw.polygon([(0, 0), (0, 10), (10, 0)], outline="yellow", fill="black")
-            elif(dist1>180 and dist1<=275):
+            elif(dist1>180 and dist1<=275 and dist1>0):
                 draw.polygon([(0, 0), (0, 15), (15, 0)], outline="yellow", fill="black")
-            elif(dist1>10 and dist1<=180):
+            elif(dist1>100 and dist1<=180 and dist1>0):
                 draw.polygon([(0, 0), (0,20), (20, 0)], outline="yellow", fill="black")
+            elif(dist1>1 and dist1<=100 and dist1>0):
+                draw.polygon([(0, 0), (0,20), (20, 0)], outline="yellow", fill="white")
                 
-
-            if(dist2<350 and dist2>275):
+            if(dist2<350 and dist2>275 and dist2>0):
                 draw.polygon([(0, 55), (0, 45), (10, 55)], outline="yellow", fill="black")
-            elif(dist2>180 and dist2<=275):
+            elif(dist2>180 and dist2<=275 and dist2>0):
                 draw.polygon([(0, 55), (0, 40), (15, 55)], outline="yellow", fill="black")
-            elif(dist2>10 and dist2<=180):
+            elif(dist2>100 and dist2<=180 and dist2>0):
                 draw.polygon([(0, 55), (0, 35), (20, 55)], outline="yellow", fill="black")
-                
-            if(dist3<350 and dist3>275):
+            elif(dist2>1 and dist2<=100 and dist2>0):
+                draw.polygon([(0, 55), (0,35), (20, 55)], outline="yellow", fill="white")
+            
+            if(dist3<350 and dist3>275 and dist3>0):
                 draw.polygon([(125, 0), (115, 0), (125, 10)], outline="yellow", fill="black")
-            elif(dist3>180 and dist3<=275):
+            elif(dist3>180 and dist3<=275 and dist3>0):
                 draw.polygon([(125, 0), (110, 0), (125, 15)], outline="yellow", fill="black")
-            elif(dist3>10 and dist3<=180):
+            elif(dist3>100 and dist3<=180 and dist3>0):
                 draw.polygon([(125, 0), (105, 0), (125, 20)], outline="yellow", fill="black")
+            elif(dist3>1 and dist3<=100 and dist3>0):
+                draw.polygon([(125, 0), (105, 0), (125, 20)], outline="yellow", fill="white")
 
-            if(dist4<350 and dist4>275):
+            if(dist4<350 and dist4>275 and dist4>0):
                 draw.polygon([(125, 55), (115, 55), (125, 45)], outline="yellow", fill="black")
-            elif(dist4>180 and dist4<=275):
+            elif(dist4>180 and dist4<=275 and dist4>0):
                 draw.polygon([(125, 55), (110, 55), (125, 40)], outline="yellow", fill="black")
-            elif(dist4>10 and dist4<=180):
+            elif(dist4>100 and dist4<=180 and dist4>0):
                 draw.polygon([(125, 55), (105, 55), (125, 35)], outline="yellow", fill="black")
-                
+            elif(dist4>1 and dist4<=100 and dist4>0):
+                draw.polygon([(125, 55), (105, 55), (125, 35)], outline="yellow", fill="white")
+
     except:
         #call the draw function again
         time.sleep(0.01)
@@ -141,14 +150,15 @@ if __name__ == "__main__":
         temp2 = get_distance(echo2, trig2)
         temp3 = get_distance(echo3, trig3)
         temp4 = get_distance(echo4, trig4)
-        if temp1>0:
-            distance1 = round(temp1)
-        if temp2>0:
-            distance2 = round(temp2)
-        if temp3>0:
-            distance3 = round(temp3)
-        if temp4>0:
-            distance4 = round(temp4)
+        temp5 = get_distance(echo5, trig5)
+        #if temp1>0:
+        distance1 = round(temp1)
+        #if temp2>0:
+        distance2 = round(temp2)
+        #if temp3>0:
+        distance3 = round(temp3)
+        #if temp4>0:
+        distance4 = round(temp4)
         
         time.sleep(0.01)
         draw_display(distance1, distance2, distance3, distance4)
