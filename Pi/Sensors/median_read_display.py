2efb45a742d6309d090272a918d81f3f449a6723
Tej Patel <tejpatel98@gmail.com>
2021-03-08 10:56:09 -0500

    Adds support for median reading on all sensors.

--- Pi/Sensors/median_read_display.py
+++ Pi/Sensors/median_read_display.py
@@ -25,10 +25,11 @@ def draw_display(dist1, dist2, dist3, dist4, dist5):
     try:
         with canvas(device) as draw:
             # First we check for 9-12 ft range, then 6-9, then 0-6.
-            #print("sensor 1 is:", dist1)
-            #print("sensor 2 is:", dist2)
-            #print("sensor 3 is:", dist3)
-            #print("sensor 4 is:", dist4)
+            print("sensor 1 is: ", dist1)
+            print("sensor 2 is: ", dist2)
+            print("sensor 3 is: ", dist3)
+            print("sensor 4 is: ", dist4)
+            print("sensor 5 is: ", dist5)
             if(dist1<350 and dist1>275 and dist1>0):
                 draw.polygon([(12.5, 6.875), (0, 13.75), (12.5, 20.625)], outline="yellow", fill="black")
             elif(dist1>180 and dist1<=275 and dist1>0):
@@ -80,15 +81,30 @@ def draw_display(dist1, dist2, dist3, dist4, dist5):
         draw_display(dist1, dist2, dist3, dist4, dist5)
 
 if __name__ == "__main__":
-    value = sensor.Measurement(trig3, echo3)
-    samples = 5 # number of samples taken at once
+    value1 = sensor.Measurement(trig1, echo1)
+    value2 = sensor.Measurement(trig2, echo2)
+    value3 = sensor.Measurement(trig3, echo3)
+    value4 = sensor.Measurement(trig4, echo4)
+    value5 = sensor.Measurement(trig5, echo5)
+    samples = 7 # number of samples taken at once
     speed = 0.1 # time between individual readings
     while True:
-        distance3 = value.raw_distance(sample_size=5, sample_wait=0.1)
-        metric_distance = value.distance_metric(distance3) # get distance in cm
+        distance1 = value1.raw_distance(sample_size=samples, sample_wait=speed)
+        distance2 = value2.raw_distance(sample_size=samples, sample_wait=speed)
+        distance3 = value3.raw_distance(sample_size=samples, sample_wait=speed)
+        distance4 = value4.raw_distance(sample_size=samples, sample_wait=speed)
+        distance5 = value5.raw_distance(sample_size=samples, sample_wait=speed)
+        metric_distance1 = value1.distance_metric(distance1) # get distance in cm
+        metric_distance2 = value2.distance_metric(distance2) # get distance in cm
+        metric_distance3 = value3.distance_metric(distance3) # get distance in cm
+        metric_distance4 = value4.distance_metric(distance4) # get distance in cm
+        metric_distance5 = value5.distance_metric(distance5) # get distance in cm
+        """
         if(metric_distance > 360):
             # out of range
             print("Out of range")
         else:
             print(distance3,"cm")
+        """
+        draw_display(metric_distance1, metric_distance2, metric_distance3, metric_distance4, metric_distance5)
         time.sleep(0.01)
